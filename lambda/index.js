38aec0bb0c50c76e7d14f311fb59d9897e8c0ce4
apeckham <apeckham@gmail.com>
2015-12-17 16:06:17 -0800

    new node-lambda proj

---
+++ lambda/index.js
@@ -0,0 +1,8 @@
+exports.handler = function( event, context ) {
+  console.log( "Running index.handler" );
+  console.log( "==================================");
+  console.log( "event", event );
+  console.log( "==================================");
+  console.log( "Stopping index.handler" );
+  context.done( );
+}
