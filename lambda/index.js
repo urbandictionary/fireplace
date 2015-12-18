8eb2df6ecff2b390c25aba9bd639342ca1a7b9d1
apeckham <apeckham@gmail.com>
2015-12-17 16:29:44 -0800

    extract term parameter

--- lambda/index.js
+++ lambda/index.js
@@ -1,5 +1,6 @@
 var elasticsearch = require('elasticsearch');
 var _ = require('lodash');
+var url = require('url');
 
 exports.handler = function (event, context) {
     console.log("==================================");
@@ -16,12 +17,19 @@ exports.handler = function (event, context) {
         type: "hit",
         body: {
             query: {filtered: {filter: {term: {page_urlpath: "/define.php"}}}},
-            fields: ["page_urlquery", "geo_country"],
+            fields: ["page_url", "geo_country"],
             size: 10,
             sort: [{_timestamp: {order: "desc"}}]
         }
     }).then(function (results) {
-        var json = _(results.hits.hits).pluck("fields").value();
+        function mapping(fields) {
+            var country = fields.geo_country[0];
+            var query = url.parse(fields.page_url[0], true).query;
+            return {country: country, term: query.term};
+        }
+
+        var json = _(results.hits.hits).pluck("fields").map(mapping).value();
+
         console.log(json);
         context.succeed(json);
     }, function (error) {
